3825c3d87e67e6afbd5618d25e4173c3f4d791bd
nicolasmr21 <nicolasmartinez502@gmail.com>
2021-04-05 09:09:15 -0500

    creting login component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule),
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule),
   },
   {
-    path: 'dashboard',
+    path: 'main',
     loadChildren: () => import('./pages/main-content/main-content.module').then(m => m.MainContentModule),
   },
 ];
