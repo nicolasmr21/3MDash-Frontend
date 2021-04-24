4868e02ec2201f1cafa0c992197b1ca5fd5263d9
nicolasmr21 <nicolasmartinez502@gmail.com>
2021-04-24 16:47:00 -0500

    display username done

--- frontend/src/app/pages/main-content/main-content.component.ts
+++ frontend/src/app/pages/main-content/main-content.component.ts
@@ -3,6 +3,7 @@ import { MENU_ITEMS, PROFILE_ITEMS } from "../../utils/items.menu";
 import { NbSidebarService } from "@nebular/theme";
 import { APP_NAME } from "../../utils/app.titles";
 import { ThemeService } from "../../services/theme.service";
+import {TokenService} from "../../services/token.service";
 
 @Component({
   selector: 'app-main-content',
@@ -15,12 +16,15 @@ export class MainContentComponent {
   title = APP_NAME;
   items = PROFILE_ITEMS;
   icon: string;
+  username: string;
 
   constructor(
     private sidebarService: NbSidebarService,
     private themeService: ThemeService,
+    private tokenService: TokenService,
   ) {
     this.icon = localStorage.getItem('theme') ? localStorage.getItem('icon') : 'moon-outline';
+    this.username = tokenService.getUserName();
   }
 
   toggle(): void {
